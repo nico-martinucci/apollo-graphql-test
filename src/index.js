2d6c62fb78f83800b99f182d36d7cb115b93f603
Nico Martinucci <domenico.martinucci@gmail.com>
2023-02-13 16:15:59 -0800

    add trial for gql axios call

--- src/index.js
+++ src/index.js
@@ -3,6 +3,58 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+import axios from 'axios';
+
+const BASE_URL = 'https://users-messages-gql.herokuapp.com/graphql';
+
+const client = new ApolloClient({
+  uri: 'https://users-messages-gql.herokuapp.com/graphql', // 
+  cache: new InMemoryCache(),
+});
+
+// const client = ...
+
+// client
+//   .query({
+//     query: gql`
+//       query GetUsers {
+//         users {
+//           username,
+//           first_name,
+//           last_name
+//         }
+//       }
+//     `,
+//   })
+//   .then((result) => console.log(result));
+
+async function getUsers() {
+  const usersQuery = `
+  query GetUsers {
+    users {
+      username,
+      first_name,
+      last_name
+    }
+  }
+`
+  const resp = await axios(
+    {
+      url: BASE_URL,
+      method: "POST",
+      data: {
+        query: usersQuery
+      },
+    }
+  )
+
+  console.log(resp.data.data.users);
+
+  return resp.data.data.users;
+}
+
+getUsers();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
